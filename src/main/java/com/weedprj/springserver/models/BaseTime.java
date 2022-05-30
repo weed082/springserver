40ec86924d962c789d8ab3987e2cac434aa573ae
weed082 <forsythia3240@gmail.com>
2022-05-30 14:23:51 +0900

    [UPDATE] JPA 생성/수정 날짜 자동 업데이트 기능 추가

---
+++ src/main/java/com/weedprj/springserver/models/BaseTime.java
@@ -0,0 +1,40 @@
+package com.weedprj.springserver.models;
+
+import java.time.LocalDateTime;
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseTime {
+  @Column(name = "upload_date")
+  @CreatedDate
+  private LocalDateTime uploadDate;
+
+  @Column(name = "update_date")
+  @LastModifiedDate
+  private LocalDateTime updateDate;
+
+  @Column(name = "delete_date")
+  private LocalDateTime deleteDate;
+
+  public LocalDateTime getUploadDate() {
+    return uploadDate;
+  }
+
+  public LocalDateTime getUpdateDate() {
+    return updateDate;
+  }
+
+  public LocalDateTime getDeleteDate() {
+    return deleteDate;
+  }
+
+  public void setDeleteDate(LocalDateTime deleteDate) {
+    this.deleteDate = deleteDate;
+  }
+}
--- src/main/java/com/weedprj/springserver/models/User.java
+++ src/main/java/com/weedprj/springserver/models/User.java
@@ -1,6 +1,7 @@
 package com.weedprj.springserver.models;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -10,45 +11,56 @@ import javax.persistence.Table;
 @Entity
 @Table(name = "user")
 @JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
-public class User {
+public class User extends BaseTime {
   @Id
   @GeneratedValue(strategy = GenerationType.AUTO)
   private int idx;
 
-  private String password;
-  private String name;
+  @Column(name = "email", nullable = false)
   private String email;
-  private String phone;
+
+  @Column(name = "password", nullable = false)
+  private String password;
+
+  @Column(name = "name", nullable = true)
+  private String name;
+
+  @Column(name = "img_url", nullable = true)
   private String imgUrl;
-
-  public User() {}
-
-  public User(String email, String password) {
-    this.email = email;
-    this.password = password;
-  }
 
   public int getIdx() {
     return idx;
-  }
-
-  public String getPassword() {
-    return password;
-  }
-
-  public String getName() {
-    return name;
   }
 
   public String getEmail() {
     return email;
   }
 
-  public String getPhone() {
-    return phone;
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  public String getPassword() {
+    return password;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
   }
 
   public String getImgUrl() {
     return imgUrl;
+  }
+
+  public void setImgUrl(String imgUrl) {
+    this.imgUrl = imgUrl;
   }
 }
