7dc7cc4f95a8e490042ccf91dfeaa3257f74f852
weed082 <forsythia3240@gmail.com>
2022-06-06 22:09:25 +0900

    [UPDATE] UserServicePort : add upload profile

--- src/main/java/com/weedprj/springserver/ports/service/UserServicePort.java
+++ src/main/java/com/weedprj/springserver/ports/service/UserServicePort.java
@@ -15,5 +15,7 @@ public interface UserServicePort {
 
   boolean existsUserByEmail(String email);
 
+  void uploadProfile(String name, String img, String firebase_token);
+
   List<User> getUsers();
 }
