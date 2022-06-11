d45fb75356bae8d438c41cf968be1e41437f70c5
weed082 <forsythia3240@gmail.com>
2022-06-12 00:34:15 +0900

    [FIX]

--- spring/src/main/java/com/weedprj/springserver/domain/user/controller/UserController.java
+++ spring/src/main/java/com/weedprj/springserver/domain/user/controller/UserController.java
@@ -29,7 +29,6 @@ public class UserController {
   @PostMapping("/user")
   @ResponseBody
   public UserDto.RegisterRes register(@RequestBody @Valid final UserDto.RegisterReq req) {
-    log.info("yes");
     return service.register(req);
   }
 
